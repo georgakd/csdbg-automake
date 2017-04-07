f68722be1b7304f62f8adb68452eb8bbac8cca72
antonis k <kalamara@projectbeagle.com>
2017-04-07 14:16:37 +0300

    example application

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # csdbg-automake
 automake for libcsdbg (for library and application)
+
+example linkage of an application with csdbg using autotools:
+
+cd example
+aclocal
+autoconf
+automake --add-missing
+./configure
+make
+./hello
+
+
---
+++ example/src/hello.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <csdbg.hpp>
+#include "hello.h"
+
+using namespace std;
+using namespace csdbg;
+
+int foo()
+{
+    throw csdbg::exception("o pateras einai anthropos");
+}
+
+int main(int argc, char **argv)
+{
+    util::init(argc, argv);
+    cout << "hello, o pateras den einai spiti\n";
+    
+    tracer *iface = tracer::interface();
+    if ( unlikely(iface == NULL) )
+        ; // abort
+    try {
+        foo();   
+    } catch (csdbg::exception &x) {
+        cout << x << "\r\n" << *iface << "\r\n";
+    }
+    iface->unwind();
+    
+}
